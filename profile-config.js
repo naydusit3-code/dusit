5aa8661a7c99a422535f2df35bb59f95fcc89b4e
naydusit3-code <naydusit3@gmail.com>
2026-09-03 14:31:04 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbxOt6ZuUv-X8hAzyXmErhUY8l_aYskTLs2ABmazi1amEX8cYS1aABwOstQPjoHcua1c1g/exec';
+  'https://script.google.com/macros/s/AKfycbxRny4dzX0EpggxrysdxDX7hfMMjvn62DCjq6keDJqKqx1Fz0vYjvgZDAn_E5XQ044EpQ/exec';
